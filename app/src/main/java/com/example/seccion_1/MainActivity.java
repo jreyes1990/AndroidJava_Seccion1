7936f9479e9707692f84c66e0ae5fc36b1043a44
jreyes1990 <jarl1990mt@gmail.com>
2023-03-26 22:14:33 -0600

    Uso de la referencia setOnClickListener.

--- app/src/main/java/com/example/seccion_1/MainActivity.java
+++ app/src/main/java/com/example/seccion_1/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+  private Button btn;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    btn = (Button) findViewById(R.id.buttonMain);
+    btn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(MainActivity.this, "Button Clicked from the code!...", Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 
-  public void miMetodo(View v){
-    Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-  }
 }
