04ecdeaab2d39ae841c416b62da17ac90cfea47a
jreyes1990 <jarl1990mt@gmail.com>
2023-04-02 19:39:39 -0600

    Uso de Intent implícito teléfono - Parte 1.

--- app/src/main/java/com/example/seccion_1/SecondActivity.java
+++ app/src/main/java/com/example/seccion_1/SecondActivity.java
@@ -2,12 +2,16 @@ package com.example.seccion_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
   private TextView textView;
+  private Button btnNext;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,7 @@ public class SecondActivity extends AppCompatActivity {
     setContentView(R.layout.activity_second);
 
     textView = (TextView) findViewById(R.id.textViewMain);
+    btnNext = (Button) findViewById(R.id.buttonGoSharing);
 
     //Tomar los datos del Intent
     Bundle bundle = getIntent().getExtras();
@@ -26,5 +31,13 @@ public class SecondActivity extends AppCompatActivity {
     }else{
       Toast.makeText(SecondActivity.this, "It is Empty!", Toast.LENGTH_SHORT).show();
     }
+
+    btnNext.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+        startActivity(intent);
+      }
+    });
   }
 }
