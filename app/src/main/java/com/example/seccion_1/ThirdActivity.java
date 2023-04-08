9db0a3d4f229f510250cd7afc1c441d7844ccd96
jreyes1990 <jarl1990mt@gmail.com>
2023-04-08 00:00:20 -0600

    Uso de Intent implícito WEB.

--- app/src/main/java/com/example/seccion_1/ThirdActivity.java
+++ app/src/main/java/com/example/seccion_1/ThirdActivity.java
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -43,8 +44,33 @@ public class ThirdActivity extends AppCompatActivity {
         if (phoneNumber != null && !phoneNumber.isEmpty()){
           // Comprobar version actual de Android que estamos corriendo
           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            // Comprobar si ha aceptado, no ha aceptado, o nunca se le ha preguntado
+            if (CheckPermission(Manifest.permission.CALL_PHONE)){
+              // Ha aceptado
+              Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("Tel: "+ phoneNumber));
+              if (ActivityCompat.checkSelfPermission(ThirdActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) return;
+              startActivity(i);
+            }else{
+              // Ha denegado, o es la primera vez que se le pregunta
+              if (!shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)){
+                // No se le ha preguntado aun
+                requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+              }else{
+                // Ha denegado
+                Toast.makeText(ThirdActivity.this, "Please, enable the request permission", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                i.addCategory(Intent.CATEGORY_DEFAULT);
+                i.setData(Uri.parse("package: "+ getPackageName()));
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                startActivity(i);
+              }
+            }
+
             // Metodo para verificar las versiones nuevas
-            requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+            // requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
           }else{
             OlderVersions(phoneNumber);
           }
@@ -63,13 +89,29 @@ public class ThirdActivity extends AppCompatActivity {
           Toast.makeText(ThirdActivity.this, "You declined the access", Toast.LENGTH_SHORT).show();
         }
       }
-
     });
+
+    // Boton para la direccion Web
+    imgBtnWeb.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String url = editTextWeb.getText().toString();
+
+        if (url != null && !url.isEmpty()){
+          // Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://"+ url));
+          Intent intentWeb = new Intent();
+          intentWeb.setAction(Intent.ACTION_VIEW);
+          intentWeb.setData(Uri.parse("http://"+ url));
+          startActivity(intentWeb);
+        }
+      }
+    });
+
   }
 
-  public void onRequestPermissionsResult(int requetsCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
     // Estamos en el caso del telefono
-    switch (requetsCode){
+    switch (requestCode){
       case PHONE_CALL_CODE:
         String permission = permissions[0];
         int result = grantResults[0];
@@ -89,12 +131,12 @@ public class ThirdActivity extends AppCompatActivity {
         }
         break;
       default:
-        super.onRequestPermissionsResult(requetsCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         break;
     }
   }
 
-  // Metodo para comprobar permisos
+  // Metodo para comprobar que el usuario haya aceptado el permiso
   private boolean CheckPermission(String permission){
     int result = this.checkCallingOrSelfPermission(permission);
     return result == PackageManager.PERMISSION_GRANTED;
